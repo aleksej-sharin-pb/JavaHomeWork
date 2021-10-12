d76b47a503d145d86435773df161d47520314c0d
Aleksej Sharin <aleksej.sharin@privatbank.ua>
2021-10-12 23:16:56 +0300

    HW1 edit out

--- src/com/pb/sharin/hw1/HelloWorld.java
+++ src/com/pb/sharin/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.sharin.hw1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
